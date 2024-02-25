42971e2abcc2f7066d724a4b09aedcc574d93f9a
kleberleite <kleberleitetavares@gmail.com>
2024-02-25 16:21:00 -0300

    feat: FolloUpCamera

---
+++ Assets/Scripts/Camera/FollowUpCamera.cs
@@ -0,0 +1,28 @@
+using AlwaysUp.Utils;
+using UnityEngine;
+
+namespace AlwaysUp.Gameplay
+{
+    [RequireComponent(typeof(Camera))]
+    public class FollowUpCamera : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private float _smooth;
+        [SerializeField] private Vector3 _offset;
+        [SerializeField] private Transform _target;
+
+        private Vector3 _targetPos = Vector3.zero;
+
+        private void FixedUpdate()
+        {
+            if (!_target)
+                return;
+
+            if (_target.position.y + _offset.y > transform.position.y)
+                _targetPos = _target.position + _offset;
+
+            transform.position = Vector3.Lerp(transform.position, _targetPos, _smooth)
+                    .With(z: transform.position.z);
+        }
+    }
+}
