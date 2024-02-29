ebfd4db29b43c45716606ae7817f01e78ecb624c
kleberleite <kleberleitetavares@gmail.com>
2024-02-29 17:24:08 -0300

    feat: better follow effect

--- Assets/Scripts/Camera/FollowUpCamera.cs
+++ Assets/Scripts/Camera/FollowUpCamera.cs
@@ -11,22 +11,43 @@ namespace AlwaysUp.Gameplay
         [SerializeField] private Vector3 _offset;
         [SerializeField] private Transform _target;
 
+        [Header("Dependencies")]
+        [SerializeField] private InputController _input;
+
+        private bool _follow = false;
+
         private Vector3 _targetPos;
+
+        private void OnEnable()
+        {
+            _input.OnJump += Enable;
+        }
+
+        private void OnEDisable()
+        {
+            _input.OnJump -= Enable;
+        }
 
         private void Awake()
         {
-            _targetPos = transform.position;
+            // -_offset is used to cancel with +_offset
+            _targetPos = transform.position - _offset;
+        }
+
+        private void Enable()
+        {
+            _follow = true;
         }
 
         private void FixedUpdate()
         {
-            if (!_target)
+            if (!_target || !_follow)
                 return;
 
-            if (_target.position.y + _offset.y > transform.position.y)
-                _targetPos = _target.position + _offset;
+            if (_target.position.y > _targetPos.y)
+                _targetPos = _target.position;
 
-            transform.position = Vector3.Lerp(transform.position, _targetPos, _smooth)
+            transform.position = Vector3.Lerp(transform.position, _targetPos + _offset, _smooth)
                     .With(z: transform.position.z);
         }
     }
