998237186941ec5eb1feacbfcba6cc90b8bc3962
kleberleite <kleberleitetavares@gmail.com>
2024-03-04 17:08:40 -0300

    feat: InputController is enable only when the scene is open

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using AlwaysUp.Events;
+using AlwaysUp.Utils;
 using UnityEngine;
 
 namespace AlwaysUp.Gameplay
 {
     public class GameManager : MonoBehaviour
     {
+        [Header("Dependencies")]
+        [SerializeField] private InputController _input;
+
         [Header("Settings")]
         [SerializeField] private float _timeBetweenBallKilledAndReset;
         [SerializeField] private Animation _openCloseScene;
+        [SerializeField] private AnimationEventHandler _onOpenScene;
 
         [Header("Broadcasting")]
         [SerializeField] private VoidEventChannelSO _reset;
@@ -26,11 +31,13 @@ namespace AlwaysUp.Gameplay
         private void OnEnable()
         {
             _onBallKilled.OnEventRaised += OnBallKilled;
+            _onOpenScene.OnEventRaised.AddListener(OnOpenScene);
         }
 
         private void OnDisable()
         {
             _onBallKilled.OnEventRaised -= OnBallKilled;
+            _onOpenScene.OnEventRaised.RemoveListener(OnOpenScene);
         }
 
         private void Start()
@@ -53,11 +60,20 @@ namespace AlwaysUp.Gameplay
         private void HandlePreparingState()
         {
             _reset.RaiseEvent();
+            _input.enabled = false;
+
             _openCloseScene.Play("OpenScene");
+        }
+
+        private void OnOpenScene()
+        {
+            _input.enabled = true;
         }
 
         private void OnBallKilled()
         {
+            _input.enabled = false;
+
             StartCoroutine(WaitDelayAndReset());
         }
 
