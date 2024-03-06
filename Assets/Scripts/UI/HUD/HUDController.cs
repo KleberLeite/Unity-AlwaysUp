95c8f7708e5fd53bac885ee9e75cf1d7327068e1
kleberleite <kleberleitetavares@gmail.com>
2024-03-06 14:57:30 -0300

    feat: better UI

--- Assets/Scripts/UI/HUD/HUDController.cs
+++ Assets/Scripts/UI/HUD/HUDController.cs
@@ -1,3 +1,4 @@
+using AlwaysUp.Events;
 using TMPro;
 using UnityEngine;
 
@@ -8,11 +9,45 @@ namespace AlwaysUp.Gameplay.UI
         [Header("Settings")]
         [SerializeField] private TMP_Text _scoreText;
         [SerializeField] private TMP_Text _highScoreText;
+        [SerializeField] private InputController _input;
+        [SerializeField] private GameObject _hudHolder;
+
+        [Header("Listening")]
+        [SerializeField] private VoidEventChannelSO _onReset;
+
+        private bool _enabled;
+
+        private void OnEnable()
+        {
+            _input.OnJump += Enable;
+            _onReset.OnEventRaised += Disable;
+        }
+
+        private void OnDisable()
+        {
+            _input.OnJump -= Enable;
+            _onReset.OnEventRaised -= Disable;
+        }
 
         private void Update()
         {
             _scoreText.text = $"Score: {ScoreController.CurrentScore}";
             _highScoreText.text = $"HighScore: {ScoreController.HighScore}";
+        }
+
+        private void Enable()
+        {
+            if (_enabled)
+                return;
+
+            _hudHolder.SetActive(true);
+            _enabled = true;
+        }
+
+        private void Disable()
+        {
+            _hudHolder.SetActive(false);
+            _enabled = false;
         }
     }
 }
